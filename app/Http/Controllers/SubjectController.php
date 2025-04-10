88cccaba8e6eb1972efcb2f7d01d1553bd30f14d
JoHigashiyama <j.higashiyama.sys23@morijyobi.ac.jp>
2025-04-11 02:28:49 +0900

    登録画面に表示する科目データを取得する処理を追記

--- app/Http/Controllers/SubjectController.php
+++ app/Http/Controllers/SubjectController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\EnrolledSubject;
+use App\Models\Subject;
 use Illuminate\Support\Facades\Auth;
+use Mockery\Matcher\Subset;
 
 class SubjectController extends Controller
 {
@@ -16,7 +18,15 @@ class SubjectController extends Controller
 
     //ユーザーの科目登録ページ
     public function user_subject_register() {
-        return view('user.user_subject_register');
+        if (!Auth::check()) {
+            // ログインしていない場合はログインページに遷移
+            return redirect('/login');
+        } else {
+            // 削除フラグがfalseの科目のみを取得する
+            $subjects = Subject::where('is_deleted', 0)->with(['user:id,name'])->get();
+            // viewに科目を渡し、リダイレクト
+            return view('user.user_subject_register', compact('subjects'));
+        }
     }
 
     //ユーザーの科目登録確認ページ
@@ -29,7 +39,7 @@ class SubjectController extends Controller
             unset($form['_token']);
             // セッションに入力された科目を登録する
             session()->put('subjects', $request->subjects);
-            // 科目をビューに渡し、リダイレクト
+            // viewに科目を渡し、リダイレクト
             $subjects = $request->subjects;
             return view('user.user_subject_register_confirm', compact('subjects'));
         }
--- app/Models/User.php
+++ app/Models/User.php
@@ -4,9 +4,12 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use App\Models\Subject;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class User extends Authenticatable
 {
@@ -44,4 +47,9 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function subjects(): HasMany
+    {
+        return $this->hasMany(Subject::class);
+    }
 }
