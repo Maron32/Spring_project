6ec86ba9ac64119e9ac964849191650daa3cbf5c
JoHigashiyama <j.higashiyama.sys23@morijyobi.ac.jp>
2025-04-10 14:10:14 +0900

    入力科目をセッションに追加、確認画面に渡す処理を追記

--- app/Http/Controllers/SubjectController.php
+++ app/Http/Controllers/SubjectController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\EnrolledSubject;
+use Illuminate\Support\Facades\Auth;
 
 class SubjectController extends Controller
 {
@@ -18,7 +20,18 @@ class SubjectController extends Controller
     }
 
     //ユーザーの科目登録確認ページ
-    public function user_subject_register_confirm() {
-        return view('user.user_subject_register_confirm');
+    public function user_subject_register_confirm(Request $request) {
+        if (!Auth::check()) {
+            // ログインしていない場合はログインページに遷移
+            return redirect('/login');
+        } else {
+            $form = $request->all();
+            unset($form['_token']);
+            // セッションに入力された科目を登録する
+            session()->put('subjects', $request->subjects);
+            // 科目をビューに渡し、リダイレクト
+            $subjects = $request->subjects;
+            return view('user.user_subject_register_confirm', compact('subjects'));
+        }
     }
 }
