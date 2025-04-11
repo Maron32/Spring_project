be08d7c138f069f1e305c11c5d01503281f06f56
JoHigashiyama <j.higashiyama.sys23@morijyobi.ac.jp>
2025-04-11 11:22:56 +0900

    seederの修正

--- database/seeders/GradesSeeder.php
+++ database/seeders/GradesSeeder.php
@@ -15,14 +15,31 @@ class GradesSeeder extends Seeder
     public function run(): void
     {
         $grades = [
-            ['name' => '1年生'],
-            ['name' => '2年生'],
-            ['name' => '3年生'],
-            ['name' => '4年生'],
+            [
+                'id' => 1,
+                'name' => '1年生',
+            ],
+            [
+                'id' => 2,
+                'name' => '2年生',
+            ],
+            [
+                'id' => 3,
+                'name' => '3年生',
+            ],
+            [
+                'id' => 4,
+                'name' => '4年生',
+            ],
+            [
+                'id' => 99,
+                'name' => '管理者'
+            ],
         ];
 
         foreach ($grades as $grade) {
             DB::table('grades')->insert([
+                'id' => $grade['id'],
                 'name' => $grade['name'],
                 'created_at' => now(),
                 'updated_at' => now(),
