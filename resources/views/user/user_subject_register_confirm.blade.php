1aa24d496b21a932092f7105a56cee7459f8168c
JoHigashiyama <j.higashiyama.sys23@morijyobi.ac.jp>
2025-04-17 13:49:51 +0900

    ユーザー科目登録確認の実装

--- resources/views/user/user_subject_register_confirm.blade.php
+++ resources/views/user/user_subject_register_confirm.blade.php
@@ -3,18 +3,33 @@
 @section('content')
 <link rel="stylesheet" href="{{ asset('css/U_subject_register_confirm.css') }}">
 
-<body>
-  <h1>ユーザーの科目登録確認画面</h1>
-  <form action="/user_subject_create" method="post" enctype="multipart/form-data">
-    @csrf
-    <p>科目名-期間</p>
-    @foreach ($subjects as $subject)
-    <p>{{ $subject->name }} - {{ $subject->term }}</p>
-    @endforeach
-    <button type="submit">登録</button>
+<body id="U_subject_confirm">
+
+  <form action="/user_subject_create" method="post" enctype="multipart/form-data" id="subjects">
+    <div class="container">
+      <h1 class="head">登録確認</h1>
+      @csrf
+      <table>
+        <tr>
+          <th>科目名</th>
+          <th>期間</th>
+        </tr>
+        @foreach ($subjects as $subject)
+        <tr>
+          <td>{{ $subject->name }}</td>
+          <td>{{ $subject->term }}</td>
+        </tr>
+        @endforeach
+      </table>
+    </div>
+    <div class="btns">
+      <button type="button" onclick="history.back()">戻る</button>
+      <button type="submit">登録</button>
+    </div>
   </form>
-  
+
   <script src="{{ asset('js/U_subject_register.js') }}" defer></script>
 </body>
+
 </html>
 @endsection
